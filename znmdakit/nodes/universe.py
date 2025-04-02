7a9596f11d87febc4195b2d07723e18818af84b7
Fabian Zills <46721498+PythonFZ@users.noreply.github.com>
2025-04-02 17:13:28 +0200

    add test repo (#3)
    * prepare for adding a test repo
    
    * add `ReadData` and remove `data_file`
    
    * fix get_bonds error
    
    * do not use networkx
    
    * add submodule
    
    * add integration test on examples repo

--- znmdakit/nodes/universe.py
+++ znmdakit/nodes/universe.py
@@ -1,7 +1,7 @@
 import logging
 from pathlib import Path
 
-import ase
+import ase.io
 import znh5md
 import zntrack
 from rdkit2ase import smiles2atoms
@@ -16,7 +16,6 @@ log = logging.getLogger(__name__)
 
 
 class Universe(zntrack.Node):
-    data_file: str = zntrack.deps_path(None)
     data: list[ase.Atoms] = zntrack.deps(None)
     residues: dict[str, str] = zntrack.params()  # dict[id, smiles]
     # overwrite: OverwriteDict = zntrack.params(default_factory=dict)
@@ -24,26 +23,17 @@ class Universe(zntrack.Node):
     frames_path: Path = zntrack.outs_path(zntrack.nwd / "frames.h5")
 
     def run(self):
-        if self.data_file is not None and self.data is not None:
-            raise ValueError("Either data_file or data must be provided")
-
         self.frames_path.parent.mkdir(parents=True, exist_ok=True)
         io = znh5md.IO(
             self.frames_path, store="time", save_units=False
         )  # ensure H5Reader can read it.
-
-        if self.data is not None:
-            io.extend(self.data)
-        else:
-            io.append(ase.Atoms("H", positions=[[0, 0, 0]]))
+        
+        io.extend(self.data)
 
     @property  # cached property needed?
     def universe(self):
         log.critical("Constructing universe")
-        if self.data_file is not None:
-            universe = get_universe(Path(self.data_file))
-        else:
-            universe = get_universe(self.frames_path)
+        universe = get_universe(self.frames_path)
 
         residues = {k: smiles2atoms(smiles=v) for k, v in self.residues.items()}
 
