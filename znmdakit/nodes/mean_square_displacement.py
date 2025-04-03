0a7af2e88d27a2b85f83230e075fb53e5f25c4ff
Fabian Zills <46721498+PythonFZ@users.noreply.github.com>
2025-04-03 08:25:54 +0200

    Update mean_square_displacement.py

--- znmdakit/nodes/mean_square_displacement.py
+++ znmdakit/nodes/mean_square_displacement.py
@@ -72,10 +72,11 @@ class SelfDiffusionFromMSD(zntrack.Node):
             raise ValueError("timestep/sampling_rate must be set")
 
         # How is the msd one shorter than the lagtimes?
-        # TODO: use loc on index and then iloc on the values
+        df_subset = self.data.results[self.start_time : self.end_time]
+
         linear_model = linregress(
-            self.data.results.index[self.start_time : self.end_time],
-            self.data.results.msd.iloc[self.start_time : self.end_time],
+            df_subset.index,
+            df_subset.msd,
         )
 
         diff = (linear_model.slope / 6) * ureg.angstrom**2 / ureg.picosecond
